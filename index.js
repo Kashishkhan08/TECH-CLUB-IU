8905faff8c451525887bd7092fb1b80965d940eb
11WALTER11 <siddarish.iul.ac.in>
2022-09-28 19:53:12 +0530

    done

---
+++ index.js
@@ -0,0 +1,111 @@
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+
+const capitalize = (str, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+    match.toUpperCase()
+  );
+
+submitBtn.addEventListener("click", () => {
+  const val = capitalize(userName.value);
+
+  //check if the text is empty or not
+  if (val.trim() !== "" && userName.checkValidity()) {
+    // console.log(val);
+    generatePDF(val);
+  } else {
+    userName.reportValidity();
+  }
+});
+
+const generatePDF = async (name) => {
+  const existingPdfBytes = await fetch("./cert.pdf").then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  pdfDoc.registerFontkit(fontkit);
+
+  //get font
+  const fontBytes = await fetch("./Sanchez-Regular.ttf").then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Embed our custom font in the document
+  const SanChezFont = await pdfDoc.embedFont(fontBytes);
+
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  // Draw a string of text diagonally across the first page
+  firstPage.drawText(name, {
+    x: 220,
+    y: 258,
+    size: 58,
+    font: SanChezFont,
+    color: rgb(0.9, 0.8, 0.68),
+  });
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save();
+  console.log("Done creating");
+
+  // this was for creating uri and showing in iframe
+
+  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  // document.getElementById("pdf").src = pdfDataUri;
+
+  var file = new File(
+    [pdfBytes],
+    "IU_IBM_Bootcamp_p_certificate.pdf",
+    {
+      type: "application/pdf;charset=utf-8",
+    }
+  );
+ saveAs(file);
+};
+
+// init();
+
+VANTA.NET({
+  el: ".body-default ",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls:true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xffc800,
+  backgroundColor:0x212529,
+
+  })
+
+
+
+
+
+//loader 
+
+var $loader = document.querySelector('.loader')
+
+window.onload = function() {
+  $loader.classList.remove('loader--active')
+};
+
+document.querySelector('.btn').addEventListener('click', function () {
+  $loader.classList.add('loader--active')
+
+  window.setTimeout(function () {
+    $loader.classList.remove('loader--active')
+  }, 5000)
+})
+
+
+
+
